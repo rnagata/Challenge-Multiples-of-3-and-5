a863e1597da5fa941180321bbf318bae202f9851
Ronald J Nagata Jr <junzo@Ronalds-MBP.home>
2019-04-02 09:30:46 -1000

    First Solution

--- multiples.js
+++ multiples.js
@@ -7,6 +7,13 @@ exports.sumOfAMultiple = function( n ) {
   var sum = 0;
 
   // do your work here
-  
+  for (let i = 1; i < n; i++){
+    if (i % 3 === 0){
+      sum += i;
+    }
+    else if (i % 5 === 0){
+      sum += i;
+    }
+  }
   return sum;
 };
